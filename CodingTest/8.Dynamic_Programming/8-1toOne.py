f7f84cc1e509f0ae619b1500b28c963501a0064e
Ssong24 <pub50@naver.com>
2021-10-22 15:01:10 +0900

    Review 8-1 toOne problem

--- CodingTest/8.Dynamic_Programming/8-1toOne.py
+++ CodingTest/8.Dynamic_Programming/8-1toOne.py
@@ -1,25 +1,31 @@
-# ** 8-1 1로 만들기 **
-# Input: 26
-# Output: 3
-def toOne():
-    n = int(input())  # 1 ~ 30,000
-    d = [0] * (n+1)
-
-    for x in range(2, n+1):
-        # Current Num - 1
-        d[x] = d[x-1] + 1
-        # Get minimum result by comparing the case x is divided by 5/3/2
-        if x % 5 == 0:
-            d[x] = min(d[x], d[x//5] + 1)
-        if x % 3 == 0:
-            d[x] = min(d[x], d[x//3] + 1)
-        if x % 2 == 0:
-            d[x] = min(d[x], d[x//2] + 1)
-
-    return d[n]
-
+# ** 1로 만들기 **
+# Input 26
+# Output 3
 import time
-start_time =  time.time()
-print(toOne())
+
+def toOne(n):
+    # Initialize the DP table
+    answer = [0] * (n+1)
+
+    for i in range(2, n+1):
+        # Calculate the one-deducted case, then compare with division of 5/3/2.
+        answer[i] = answer[i-1] + 1
+
+        # If num is divided by 5 or 3 or 2, compare its calculation count with each one.
+        if i % 5 == 0:
+            answer[i] = min(answer[i//5] + 1, answer[i])
+        if i % 3 == 0:
+            answer[i] = min(answer[i//3] + 1, answer[i])
+        if i % 2 == 0:
+            answer[i] = min(answer[i//2] + 1, answer[i])
+
+    print(answer[n])
+
+# Get input
+n = int(input()) # 1 ~ 30,000
+
+# Measure the time (limit: 1 sec)
+start_time = time.time()
+toOne(n)
 end_time = time.time()
-print("Time: {}[sec]".format(end_time - start_time))
+print("Time: {:.4f}[sec]".format(end_time - start_time))
