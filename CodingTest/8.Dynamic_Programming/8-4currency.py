b1aba856947a7a88f2309c05e8453e253d7b7691
Ssong24 <pub50@naver.com>
2021-10-23 21:12:38 +0900

    Review 8-4 currency

--- CodingTest/8.Dynamic_Programming/8-4currency.py
+++ CodingTest/8.Dynamic_Programming/8-4currency.py
@@ -1,23 +1,32 @@
-# 효율적인 화폐 구성
+import time
+
+
 def currency(m, coins):
-    matrix = [10001] * (m+1)
-    matrix[0] = 0
-    coins.sort()
+    # Initialize DP table
+    MAX = 10001
+    count = [MAX] * (m + 1)
 
-    init_point = coins[0]
-    for k in coins:
-        for i in range(init_point, m+1):
-            matrix[i] = min(matrix[i], matrix[i-k] + 1)
+    # Omit the coin less than m
+    coins = [coin for coin in coins if coin <= m]
 
-    if matrix[m] != 10001:
-        return matrix[i]
-    else:
-        return -1
+    for coin in coins:
+        count[coin] = 1
+        # Bottom-Up DP
+        for i in range(coin+1, m+1):
+            count[i] = min(count[i], count[i-coin] + 1)
 
-# Get input
-# n, m = map(int, input().split())
-# coins = []
+    answer = count[m]
+    if answer == MAX:
+        answer = -1
+    return answer
+
+n, m = 100, 10000  # map(int, input().split())
+
+coins= [x for x in range(1, n+1)] 
+print(coins)
 # for i in range(n):
 #     coins.append(int(input()))
-answer = currency(4, [3,5,7])
-print(answer) # -1
+start = time.time()
+print(currency(m, coins))
+end = time.time()
+print("Time: {:5f}".format(end - start))  # limit: 1[sec]
