a9f05f323d931a30110e961d537d5a627dc3e146
Ssong24 <pub50@naver.com>
2021-11-11 09:57:12 +0900

    Update level1_lotto.py

--- programmers/level1_lotto.py
+++ programmers/level1_lotto.py
@@ -1,27 +1,18 @@
 def solution(lottos, win_nums):
-    answer = []  # 최저, 최고 순위를 담을 배열
-    prizeList = [6, 6, 5, 4, 3, 2, 1]  # 당첨된 번호 갯수를 인덱스해서 순위 저장한 배열
+    # 맞춘 갯수에 따른 등수
+    rank = [6, 6, 5, 4, 3, 2, 1]
     
-    lottoMax = 45  # 로또에서 가능한 최대 숫자
-    check = [False] * (lottoMax + 1)  # 당첨된 번호의 결과를 저장
-    unKnown = 0
+    # 모르는 숫자 갯수와 맞춘 숫자 갯수 초기화
+    unKnown, correctCnt = 0, 0
     
-    # 당첨 번호는 True
-    for l in win_nums:
-        check[l] = True
-    
-    # 로또 당첨된 번호 갯수 계산
-    correctCnt = 0
-    for n in lottos:
-        if n == 0:
-            unKnown += 1
-        elif check[n]:
+    for l in lottos:
+        if l in win_nums:
             correctCnt += 1
-            
-    # 최고 순위 번호
-    answer.append(prizeList[correctCnt + unKnown])
-    # 최저 순위 번호     
-    answer.append(prizeList[correctCnt])
+        elif l == 0:
+            unKnown += 1
     
+    # 최고 순위, 최저 순위
+    highest, lowest = rank[correctCnt + unKnown], rank[correctCnt]
     
+    answer = [highest, lowest]
     return answer
