812ca4fad012609662321c8b576b196ebfbf89bd
Ssong24 <pub50@naver.com>
2021-11-11 10:07:30 +0900

    Update level1_newID.py

--- programmers/level1_newID.py
+++ programmers/level1_newID.py
@@ -1,42 +1,39 @@
 def solution(new_id):
     answer = ''
-    # stage 1
+    # 1
     new_id = new_id.lower()
-
-    # stage 2
+    
+    # 2 ** ASCII_0에 ASCII_9를 넣어버림
+    ASCII_a, ASCII_z = ord('a'), ord('z')
+    ASCII_0, ASCII_9 = ord('0'), ord('9')
     for l in new_id:
-        if ord("a") <= ord(l) <= ord("z"):
-            answer+=l
-        elif ord('0') <= ord(l) <= ord('9'):
+        if ASCII_a <= ord(l) <= ASCII_z:
             answer += l
-        elif l in ["-", "_", "."]:
-            answer+=l
+        elif ASCII_0 <= ord(l) <= ASCII_9:
+            answer += l
+        elif l in ['-', '_', '.']:
+            answer += l
     
-    # stage 3
+    # 3
     while '..' in answer:
-        answer = answer.replace("..", ".")
-
-    
-    # stage 4
+        answer = answer.replace('..', '.')
+    # 4
     if len(answer) > 0 and answer[0] == '.':
         answer = answer[1:]
     if len(answer) > 0 and answer[-1] == '.':
         answer = answer[:-1]
     
-    # stage 5
+    # 5
     if len(answer) == 0:
-        answer += "a"
-        
-    # stage 6
-    limit=16
-    if len(answer) >= limit:
-        answer = answer[:limit-1]
-        if answer[-1] =='.':
+        answer += 'a'
+    # 6
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
             answer = answer[:-1]
-            
-    # stage 7
-    if len(answer) <= 2:
-        while len(answer) < 3:
-            answer += answer[-1]
-            
+    
+    # 7 ** 지문 읽다가 len(answer)에 len(new_id) 넣어버림
+    while len(answer) <= 2:
+        answer += answer[-1]
+    
     return answer
