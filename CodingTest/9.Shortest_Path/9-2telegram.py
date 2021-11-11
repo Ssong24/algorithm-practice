15a9cfdf6f5d0eb43a51ebf7eeca2ab1a1ca1bc3
Ssong24 <pub50@naver.com>
2021-11-11 09:46:06 +0900

    Update 9-2telegram.py

--- CodingTest/9.Shortest_Path/9-2telegram.py
+++ CodingTest/9.Shortest_Path/9-2telegram.py
@@ -1,50 +1,59 @@
-import heapq
+"""
+다익스트라 / 플로이드 워셜 (FW)
+노드 갯수, 간선 갯수
+경로 정보 담는 그래프 (FW: cost 담음)
+최초 지점과의 거리 정보 담는 리스트(다익스트라)
+자료구조: 최소 힙
+"""
+import heapq as hq
 import sys
 input = sys.stdin.readline
+INF = int(1E+9)
 
-INF = int(1e9)
+v, e, start = map(int, input().split())  # 5, 3, 1
+graph = [[] for _ in range(v+1)]
+distance = [INF] * (v+1)
 
-n, m, start = 5,3, 1 # map(int, input().split())
-# Initialize Graph (edge info)
-graph = [[] for _ in range(n+1)]
-# Initialize distance (start to specific node)
-distance = [INF] * (n+1)
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    graph[a].append([b, cost])  # 이웃노드, 비용
 
-graph[1].append([2,1])
-graph[1].append([3,2])
-graph[1].append([5,1])
-graph[2].append([4,3])
-graph[4].append([5,1])
-# Get input of graph info
-# for _ in range(m):
-#     a, b, cost = map(int, input().split())
-#     graph[a].append([a, b])  # 이웃 노드, 비용
 
 def dijkstra(start, graph, distance):
-    q = []
-    heapq.heappush(q, (0, start))
+    h = []
     distance[start] = 0
+    hq.heappush(h, (0, start))
 
-    while q:
-        dist, now = heapq.heappop(q)
-
-        if distance[now] < dist:
-            continue
+    while h:
+        nowCost, now = hq.heappop(h)
         for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+            next, nextCost = i[0], i[1]
+            allCost = nowCost + nextCost
+            if allCost < distance[next]:
+                distance[next] = allCost
+                hq.heappush(h, (allCost, next))
 
-    count, time = 0, 0
-    for i in range(1, n+1):
-        if distance[i] != INF:
-            count += 1
-            if time < distance[i]:
-                time = distance[i]
-    count -= 1  # start 노드 빼주기
 
-    return count, time
+def telegram(start, graph, distance):
+    dijkstra(start, graph, distance)
+    answer = 0
+    n_city = 0
+    for d in distance:
+        if d != INF:
+            answer = max(answer, d)
+            n_city += 1
 
-count, time = dijkstra(start, graph, distance)
-print(count, time)  # 4, 4
+    answer -= 1  # start 노드 빼주기
+    print(n_city, answer)
+
+telegram(start, graph, distance)
+
+
+# 그래프 input
+# graph[1].append([2,1])
+# graph[1].append([3,2])
+# graph[1].append([5,1])
+# graph[2].append([4,3])
+# graph[4].append([5,1])
+# ouput
+# 4 4
